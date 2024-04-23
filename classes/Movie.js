e048eeb540acf321c4c5547d4f6a6fc95469fa86
Nicole Rojas <63315208+NPRojas@users.noreply.github.com>
2024-04-23 15:02:36 -0400

    complete music class

--- classes/Movie.js
+++ classes/Movie.js
@@ -26,15 +26,3 @@ class Movie extends Media {
 }
 // don't change below
 module.exports = Movie;
-
-/*
-1. In `Movie.js`, create a `Movie` class. The `Movie` class should be a subclass of `Media`. Import `Media` into the `Movie.js` file using `require`. 
-2. `Music` should have the following properties and methods:
-    - `director`: the director of the movie (string)
-    - `duration`: the duration of the movie in minutes (number)
-    - `rating`: the average rating of the movie (number between 1-5)
-    - `summary()`: a method that returns a summary of the movie, including the director, duration, and rating (string) as `"Title: <TITTLE>, Director: <DIRECTOR>, Year: <YEAR>, Genre: <GENRE>, Duration: <DURATION>, Rating: <RATING>"`
-3. The `Movie` class should have a static method `longestMovie` that takes an array of `Movie` objects and returns the `Movie` with the longest duration.
-4. Export the `Movie` class using `module.exports`.
-5. In `index.js`, import `Movie` using `require`.
-*/
