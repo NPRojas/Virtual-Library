f655dbe55fe8313ba60524c3c6e185bf2854ae5f
rxshellg <rashellguerrero123@gmail.com>
2024-04-23 14:40:57 -0400

    Completed Music class

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 // Import classes here to console.log and debug
 const Media = require("./classes/Media.js");
-const Book = require("./classes/Book.js")
+const Book = require("./classes/Book.js");
+const Movie = require("./classes/Movie.js")
